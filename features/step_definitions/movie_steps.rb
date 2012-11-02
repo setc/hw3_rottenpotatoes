0c10af0ce763c955ad134c4170e3d9e174965d4d
Sebastian Torrente <sebastian.torrente@gmail.com>
2012-11-02 13:26:45 -0700

    Steps added

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,27 +2,52 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  assert movies_table.hashes.size == Movie.all.count
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  titles = page.all("table#movies tbody tr td[1]").map {|t| t.text}
+  assert titles.index(e1) < titles.index(e2)
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(",").each do |field|
+    field = field.strip
+    if uncheck == "un"
+       step %Q{I uncheck "ratings_#{field}"}
+       step %Q{the "ratings_#{field}" checkbox should not be checked}
+    else
+      step %Q{I check "ratings_#{field}"}
+      step %Q{the "ratings_#{field}" checkbox should be checked}
+    end
+  end
+end
+ 
+Then /^I should see the following ratings: (.*)/ do |rating_list|
+  ratings = page.all("table#movies tbody tr td[2]").map! {|t| t.text}
+  rating_list.split(",").each do |field|
+    assert ratings.include?(field.strip)
+  end
+end
+ 
+Then /^I should not see the following ratings: (.*)/ do |rating_list|
+  ratings = page.all("table#movies tbody tr td[2]").map! {|t| t.text}
+  rating_list.split(",").each do |field|
+    assert !ratings.include?(field.strip)
+  end
+end
+ 
+Then /^I should see all of the movies$/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert ( rows.size == Movie.all.count )
+end
+ 
+Then /^I should see no movies$/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert rows.size == 0
 end
